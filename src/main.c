396cd4596b3b46006373a913e91d7d1774adf42a
dgomez0417 <gmdenies@miners.utep.edu>
2021-03-04 10:11:41 -0700

    updated main with history calls

--- src/main.c
+++ src/main.c
@@ -12,18 +12,21 @@ int main()
 
   printf("space char: %d \n", space_char(input[1]));
   printf("non space char: %d \n", non_space_char(input[1]));
-  //printf("%d \n", word_start(input));
-  // printf("%d \n", word_terminator(input));
   printf("word count: %d \n", count_words(input));
   printf("copy of string: %s \n", copy_str(input, 100));
   char **token = tokenize(input);
   print_tokens(token);
   free_tokens(token);
 
+  
   List *list = (init_history());
   add_history(list, input);
-  //printf("%s \n", get_history(list, 2));
-  //print_history(list);
+  
+  printf("should be get history \n");
+  printf("%s \n", get_history(list, 1));
+
+  //printf("should be print histroy \n");
+  // print_history(list);
   
 }
 
