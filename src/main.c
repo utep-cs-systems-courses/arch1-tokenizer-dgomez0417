2a3dd9dc941765f36000d663d249a51386f36b9a
dgomez0417 <gmdenies@miners.utep.edu>
2021-03-03 17:18:48 -0700

    added history to the main

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "tokenizer.h"
+#include "history.h"
 
 int main()
 {
@@ -9,8 +10,20 @@ int main()
   fgets(input, 100, stdin);
   puts(input);
 
-  printf("%d \n",space_char(input[1]));
-  printf("%d \n",non_space_char(input[1]));
-  printf("%d \n", count_words(input));
+  printf("space char: %d \n", space_char(input[1]));
+  printf("non space char: %d \n", non_space_char(input[1]));
+  //printf("%d \n", word_start(input));
+  // printf("%d \n", word_terminator(input));
+  printf("word count: %d \n", count_words(input));
+  printf("copy of string: %s \n", copy_str(input, 100));
+  char **token = tokenize(input);
+  print_tokens(token);
+  free_tokens(token);
+
+  List *list = (init_history());
+  add_history(list, input);
+  //printf("%s \n", get_history(list, 2));
+  //print_history(list);
+  
 }
 
