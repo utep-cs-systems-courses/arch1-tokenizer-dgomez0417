d0453f808a6031a275716b742f2251f3af3b057f
dgomez0417 <gmdenies@miners.utep.edu>
2021-03-04 10:09:44 -0700

    get history wont work anymore

--- src/history.c
+++ src/history.c
@@ -13,7 +13,6 @@ List* init_history(){
    char* str - the string to store
 */
 void add_history(List *list, char *str){
-  char *clone;
   Item *newnode = malloc(sizeof(Item));
   if(list->root == NULL){
     newnode->id = 1;
@@ -43,25 +42,28 @@ char *get_history(List *list, int id){
   while(temp != NULL){
     if(temp->id == id){
       str = temp->str;
+      return str;
     }
     temp = temp->next;
   }
-  return str;
+  return 0;
 }
 
 /*Print the entire contents of the list. */
 void print_history(List *list){
   Item * temp = list->root;
-  while(temp->next != 0){
-    printf("%d. %s", temp->id, temp->str);
+  while(temp->next != NULL){
+    printf("%d. %s \n", temp->id, temp->str);
     temp = temp->next;
   }
 }
   
 /*Free the history list and the strings it references. */
 void free_history(List *list){
-  //Item * temp = list->root;
-  //while(temp->next != 0){
-    //free(temp->id, temp->str);
-    //temp = temp->next;
+  Item * temp = list->root;
+  while(temp->next != NULL){
+    free(temp->str);
+    temp = temp->next;
+  }
+  free(list);
 }
